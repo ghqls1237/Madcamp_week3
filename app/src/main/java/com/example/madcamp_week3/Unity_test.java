edb64dd2912bbc22c81fe35dc565a020b42ed94f
DESKTOP-QITT7RJ\q <ghqls1237@kaist.ac.kr>
2021-01-14 23:52:56 +0900

    unity test #3

---
+++ app/src/main/java/com/example/madcamp_week3/Unity_test.java
@@ -0,0 +1,14 @@
+package com.example.madcamp_week3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Unity_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unity_test);
+    }
+}

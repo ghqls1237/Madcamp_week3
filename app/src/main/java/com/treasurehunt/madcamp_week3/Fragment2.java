e8c31d533e537aa548e02e50d347de90d26de6f9
DESKTOP-QITT7RJ\q <ghqls1237@kaist.ac.kr>
2021-01-18 16:06:59 +0900

    unity integration, but camera not working

--- app/src/main/java/com/treasurehunt/madcamp_week3/Fragment2.java
+++ app/src/main/java/com/treasurehunt/madcamp_week3/Fragment2.java
@@ -1,5 +1,6 @@
 package com.treasurehunt.madcamp_week3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,16 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+        Button button = view.findViewById(R.id.gamestart);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UnityPlayerActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+        return view;
     }
 }
